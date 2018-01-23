10359b13fd2cdba58b5cc3edf3d3128edc9227f7
yvesandre-middleton <yvesmiddleton@gmail.com>
2018-01-23 21:30:25 +0000

    added a post request to a new page and added a function that creates a random alpha string

--- express_server.js
+++ express_server.js
@@ -1,7 +1,24 @@
 var express = require("express");
 var app = express();
 var PORT = process.env.PORT || 8080; // default port 8080
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
 
+function generateRandomString() {
+   const vocabulary = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'x', 'y', 'z'];
+   let output = "";
+   for (let i = 0; i < 6; i++) {
+       let index = getRandomInt();
+       output += vocabulary[index];
+   }
+   return output;
+}
+
+function getRandomInt() {
+   min = Math.ceil(0);
+   max = Math.floor(26);
+   return Math.floor(Math.random() * (26 - 0)) + 0;
+}
 
 app.set("view engine", "ejs");
 
@@ -9,6 +26,15 @@ var urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // debug statement to see POST parameters
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+});
 
 app.get("/urls/:id", (req, res) => {
   let templateVars = { shortURL: req.params.id, urls: urlDatabase};
